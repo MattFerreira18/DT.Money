948c2e90a2f1f96bf7b994081b5bb9572e0136a4
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-02-10 22:51:43 -0300

    feat: implemented the api in profile page

--- frontend/src/ui/pages/Profile/index.tsx
+++ frontend/src/ui/pages/Profile/index.tsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from 'react';
+
+import { api } from '../../../app/services/api';
 import { format } from '../../../utils/format';
 import { SubmitButton } from '../../components/buttons';
 import { Input } from '../../components/Input';
 
 import { Container, EditProfile, Statistic } from './styles';
 
+interface UserData {
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+interface StatisticsData {
+  total_transactions: number;
+  total_deposits: number;
+  total_withdraws: number;
+}
+
+interface UserResponseData {
+  user: UserData;
+}
+
 export function Profile() {
+  const [userData, setUserData] = useState({} as UserData);
+  const [statisticsData, setStatisticsData] = useState({} as StatisticsData);
   const [isEditingPassword, setIsEditingPassword] = useState(false);
   const [newPassword, setNewPassword] = useState('');
 
@@ -17,28 +37,46 @@ export function Profile() {
     }
   }, [newPassword]);
 
+  useEffect(() => {
+    api.get<UserResponseData>('/users/profile')
+      .then(response => {
+        if (response.status === 200) {
+          const { user } = response.data;
+
+          setUserData(user);
+        }
+      });
+
+    api.get<StatisticsData>('/transactions/statistics')
+      .then(response => {
+        if (response.status === 200) {
+          setStatisticsData(response.data);
+        }
+      })
+  }, []);
+
   return (
     <Container>
       <div>
         <Statistic>
           <h5>Total de Entradas</h5>
-          <strong className='deposits'>{format.amount(1500)}</strong>
+          <strong className='deposits'>{format.amount(statisticsData.total_deposits)}</strong>
         </Statistic>
         <Statistic>
           <h5>Total de Saida</h5>
-          <strong className='withdraws'>{format.amount(720)}</strong>
+          <strong className='withdraws'>{format.amount(statisticsData.total_withdraws)}</strong>
         </Statistic>
         <Statistic>
           <h5>Total de Produtos Cadastrados</h5>
-          <strong>600</strong>
+          <strong>{statisticsData.total_transactions}</strong>
         </Statistic>
       </div>
 
       <EditProfile>
         <form>
           <h2>Perfil</h2>
-          <Input value="Matheus Ferreira" placeholder='Nome' onChange={() => { }} />
-          <Input value="matheusferreira.dev@gmail.com" placeholder='Email' type="email" onChange={() => { }} />
+          <Input value={userData.name} placeholder='Nome' onChange={() => { }} />
+          <Input value={userData.email} placeholder='Email' type="email" onChange={() => { }} />
           <Input onChange={(e) => setNewPassword(e.target.value)} placeholder="Nova senha" type="password" value={newPassword} />
 
           {
